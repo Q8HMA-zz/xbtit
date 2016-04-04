d334eeeec4d2088a1cfa8e8d1c14c7558bebe4ce
Alan <Alan.Byrne@dpwsmedia.com>
2016-04-04 16:58:27 +0100

    First Commit

---
+++ include/conextra.php
@@ -0,0 +1,33 @@
+<?php
+
+function getConnection () {
+
+    $CURRENTPATH = dirname(__FILE__);
+
+    include($CURRENTPATH."/settings.php"); # contains the given DB setup $database, $dbhost, $dbuser, $dbpass
+    
+    $Econn = ($GLOBALS["___mysqli_ston"] = mysqli_connect($dbhost,  $dbuser,  $dbpass));
+    if (!$Econn) {
+            echo "Connection to DB was not possible!";
+            end;
+        }
+        if (!((bool)mysqli_query( $Econn, "USE $database"))) {
+            echo "No DB with that name seems to exist on the server!";
+            end;
+        }
+        
+        if(isset($GLOBALS["charset"]) && $GLOBALS["charset"]=="UTF-8")
+            mysqli_query($GLOBALS["___mysqli_ston"], "SET NAMES utf8");
+        
+        return $Econn;
+}
+
+function getPrefix () {
+
+    $CURRENTPATH = dirname(__FILE__);
+
+    include($CURRENTPATH."/settings.php"); # contains the given DB setup $database, $dbhost, $dbuser, $dbpass
+    
+    return $TABLE_PREFIX;
+}
+?>
---
+++ include/xbtit_version.php
@@ -0,0 +1,5 @@
+<?php
+$tracker_version='2.4.0'; # version
+$tracker_revision='758';  # revision
+$kc_svn_revision='23';  # revision
+?>
