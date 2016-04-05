c38f8553fa5c2c1f6144f487a6f533f22beedd44
Alan <Alan.Byrne@dpwsmedia.com>
2016-04-05 05:21:00 +0100

    Start Process to default theme and fixes

--- blocks/dropdownmenu_block.php
+++ blocks/dropdownmenu_block.php
@@ -35,142 +35,32 @@
 ////////////////////////////////////////////////////////////////////////////////////
 global $CURUSER, $btit_settings, $language;
 
-print("<div id='menu'>\n<ul class='level1'>");
+print("<div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>");
+print("<ul class='nav navbar-nav navbar-right'>");
+
+print("<li><a href='index.php'>".$language["MNU_INDEX"]."</a></li>\n");
          
 if ($CURUSER["view_torrents"]=="yes")    
 {    
-    print("<li class='level1-li'><a class='level1-a drop' href='#'>".$language['TORRENT_MENU']."</a>\n");
-    print("<!--[if gte IE 7]><!--></a><!--<![endif]--><!--[if lte IE 6]><table><tr><td><![endif]-->\n");
-    print("<ul class='level2'>\n");
-		
-		print("<li><a class='fly' href='#'>".$language["MNU_TORRENT"]."</a>\n");
-    print("<!--[if gte IE 7]><!--></a><!--<![endif]--><!--[if lte IE 6]><table><tr><td><![endif]-->\n");
-    print("<ul class='level3'>\n");
+	print("<li class='dropdown'><a href='#' class='dropdown-toggle' data-toggle='dropdown'>".$language["MNU_TORRENT"]."<b class='caret'></b></a>\n");
+    print("<ul class='dropdown-menu'>\n");
     print("<li><a href='index.php?page=torrents&search=&category=0&active=0'>".$language["ALL"]."</a></li>\n");
     print("<li><a href='index.php?page=torrents&search=&category=0&active=1'>".$language["ACTIVE_ONLY"]."</a></li>\n");
     print("<li><a href='index.php?page=torrents&search=&category=0&active=2'>".$language["DEAD_ONLY"]."</a></li>\n");
-    print("</ul><!--[if lte IE 6]></td></tr></table></a><![endif]--></li>");
-		
+
 if ($CURUSER["can_upload"]=="yes")                
 {
-    print("<li><a class='fly' href='#'>".$language['UPLOAD_LINK']."</a>\n");
-    print("<!--[if gte IE 7]><!--></a><!--<![endif]--><!--[if lte IE 6]><table><tr><td><![endif]-->\n");
-    print("<ul class='level3'>\n");
+
     print("<li><a href='index.php?page=upload'>".$language["MNU_UPLOAD"]."</a></li>\n");
-    print("</ul><!--[if lte IE 6]></td></tr></table></a><![endif]--></li>");
-}
-		
-    print("<li><a href='index.php?page=extra-stats'>".$language["MNU_STATS"]."</a></li>\n");    
-}
-                
-// This is not needed, is why I commented it out!!
-if($CURUSER["view_torrents"]=="yes" || $CURUSER["can_upload"]=="yes")
-{
-    print("</ul><!--[if lte IE 6]></td></tr></table></a><![endif]--></li>");
-}
 
-print("<li class='level1-li'><a href='index.php'>".$language["MNU_INDEX"]."</a></li>\n");
+}	
+	
+    print("</ul></li>");
 
-if ($CURUSER["uid"]==1 || !$CURUSER)
-{
-    print("<li class='level1-li'><a href='index.php?page=login'>".$language["LOGIN"]."</a></li>\n");
-}
-else
-{
-    print("<li class='level1-li'><a href='logout.php'>".$language["LOGOUT"]."</a></li>\n");
-}
+    print("<li><a href='index.php?page=extra-stats'>".$language["MNU_STATS"]."</a></li>\n");                  
 
-if ($CURUSER["view_news"]=="yes")
-{
-    print("<li class='level1-li left'><a class='level1-a drop' href='#'>".$language['USER_MENU']."</a>\n");
-    print("<!--[if lte IE 6]><table><tr><td><![endif]-->\n");
-    print("<ul class='level2'>\n");
-		
-    if ($CURUSER["admin_access"]=="yes")
-    {
-        require_once(load_language("lang_admin.php"));
-        print("<li><a class='fly' href='#'>".$language["ADMIN_ACCESS"]."</a>\n");
-        print("<!--[if gte IE 7]><!--></a><!--<![endif]--><!--[if lte IE 6]><table><tr><td><![endif]-->\n");
-        print("<ul class='level3'>\n");
-        print("<li><a href='index.php?page=admin&amp;user=".$CURUSER["uid"]."&amp;code=".$CURUSER["random"]."'>".$language["MNU_ADMINCP"]."</a></li>");
-        
-        print("<li><a class='fly' href='#'>".$language['TRACKER_SETTINGS']."</a>\n");
-        print("<!--[if gte IE 7]><!--></a><!--<![endif]--><!--[if lte IE 6]><table><tr><td><![endif]-->\n");
-        print("<ul class='level4'>\n");
-        print("<li><a href='index.php?page=admin&amp;user=".$CURUSER["uid"]."&amp;code=".$CURUSER["random"]."&do=config&action=read'>".$language['TRACKER_SETTINGS']."</a></li>\n");
-        print("<li><a href='index.php?page=admin&amp;user=".$CURUSER["uid"]."&amp;code=".$CURUSER["random"]."&do=banip&action=read'>".$language['ACP_BAN_IP']."</a></li>\n");
-        print("<li><a href='index.php?page=admin&amp;user=".$CURUSER["uid"]."&amp;code=".$CURUSER["random"]."&do=language&action=read'>".$language['ACP_LANGUAGES']."</a></li>\n");
-        print("<li><a href='index.php?page=admin&amp;user=".$CURUSER["uid"]."&amp;code=".$CURUSER["random"]."&do=style&action=read'>".$language['ACP_STYLES']."</a></li>\n");
-        print("<li><a href='index.php?page=admin&amp;user=".$CURUSER["uid"]."&amp;code=".$CURUSER["random"]."&do=security_suite'>".$language["ACP_SECSUI_SET"]."</a></li></ul><!--[if lte IE 6]></td></tr></table></a><![endif]--></li>\n");
-        
-        print("<li><a class='fly' href='#'>".$language['ACP_FRONTEND']."</a>\n");
-				print("<!--[if gte IE 7]><!--></a><!--<![endif]--><!--[if lte IE 6]><table><tr><td><![endif]-->\n");
-        print("<ul class='level4'>\n");
-        print("<li><a href='index.php?page=admin&amp;user=".$CURUSER["uid"]."&amp;code=".$CURUSER["random"]."&do=category&action=read'>".$language['ACP_CATEGORIES']."</a></li>\n");
-        print("<li><a href='index.php?page=admin&amp;user=".$CURUSER["uid"]."&amp;code=".$CURUSER["random"]."&do=poller&action=read'>".$language['ACP_POLLS']."</a></li>");
-        print("<li><a href='index.php?page=admin&amp;user=".$CURUSER["uid"]."&amp;code=".$CURUSER["random"]."&do=badwords&action=read'>".$language["ACP_CENSORED"]."</a></li>");
-        print("<li><a href='index.php?page=admin&amp;user=".$CURUSER["uid"]."&amp;code=".$CURUSER["random"]."&do=blocks&action=read'>".$language['ACP_BLOCKS']."</a></li></ul><!--[if lte IE 6]></td></tr></table></a><![endif]--></li>\n");
-        
-        print("<li><a class='fly' href='#'>".$language['ACP_USERS_TOOLS']."</a>\n");
-				print("<!--[if gte IE 7]><!--></a><!--<![endif]--><!--[if lte IE 6]><table><tr><td><![endif]-->\n");
-        print("<ul class='level4'>\n");
-        print("<li><a href='index.php?page=admin&amp;user=".$CURUSER["uid"]."&amp;code=".$CURUSER["random"]."&do=groups&action=read'>".$language['ACP_USER_GROUP']."</a></li>\n");
-        print("<li><a href='index.php?page=admin&amp;user=".$CURUSER["uid"]."&amp;code=".$CURUSER["random"]."&do=masspm&action=write'>".$language['ACP_MASSPM']."</a></li>\n");
-        print("<li><a href='index.php?page=admin&amp;user=".$CURUSER["uid"]."&amp;code=".$CURUSER["random"]."&do=pruneu'>".$language['ACP_PRUNE_USERS']."</a></li>\n");
-        print("<li><a href='index.php?page=admin&amp;user=".$CURUSER["uid"]."&amp;code=".$CURUSER["random"]."&do=searchdiff'>".$language['ACP_SEARCH_DIFF']."</a></li></ul><!--[if lte IE 6]></td></tr></table></a><![endif]--></li>\n");
-        
-        print("<li><a class='fly' href='#'>".$language['ACP_TORRENTS_TOOLS']."</a>\n");
-				print("<!--[if gte IE 7]><!--></a><!--<![endif]--><!--[if lte IE 6]><table><tr><td><![endif]-->\n");
-        print("<ul class='level4'>\n");
-        print("<li><a href='index.php?page=admin&amp;user=".$CURUSER["uid"]."&amp;code=".$CURUSER["random"]."&do=prunet'>".$language['ACP_PRUNE_TORRENTS']."</a></li></ul><!--[if lte IE 6]></td></tr></table></a><![endif]--></li>\n");
-        
-        print("<li><a class='fly' href='#'>".$language['ACP_FORUM']."</a>\n");
-				print("<!--[if gte IE 7]><!--></a><!--<![endif]--><!--[if lte IE 6]><table><tr><td><![endif]-->\n");
-        print("<ul class='level4'>\n");
-        print("<li><a href='index.php?page=admin&amp;user=".$CURUSER["uid"]."&amp;code=".$CURUSER["random"]."&do=forum&action=read'>".$language['ACP_FORUM']."</a></li></ul><!--[if lte IE 6]></td></tr></table></a><![endif]--></li>\n");
-        
-        print("<li><a class='fly' href='#'>".$language['ACP_OTHER_TOOLS']."</a>");
-        print("<ul class='level4'>\n");
-        print("<li><a href='index.php?page=admin&amp;user=".$CURUSER["uid"]."&amp;code=".$CURUSER["random"]."&do=dbutil'>".$language['ACP_DBUTILS']."</a></li>");
-        print("<li><a href='index.php?page=admin&amp;user=".$CURUSER["uid"]."&amp;code=".$CURUSER["random"]."&do=mysql_stats'>".$language['ACP_MYSQL_STATS']."</a></li>");
-        print("<li><a href='index.php?page=admin&amp;user=".$CURUSER["uid"]."&amp;code=".$CURUSER["random"]."&do=logview'>".$language['ACP_SITE_LOG']."</a></li></ul><!--[if lte IE 6]></td></tr></table></a><![endif]--></li>\n");
-        
-        print("<li><a class='fly' href='#'>".$language['ACP_MODULES']."</a>\n");
-				print("<!--[if gte IE 7]><!--></a><!--<![endif]--><!--[if lte IE 6]><table><tr><td><![endif]-->\n");
-        print("<ul class='level4'>\n");
-        print("<li><a href='index.php?page=admin&amp;user=".$CURUSER["uid"]."&amp;code=".$CURUSER["random"]."&do=module_config&action=manage'>".$language['ACP_MODULES_CONFIG']."</a></li></ul><!--[if lte IE 6]></td></tr></table></a><![endif]--></li>\n");
-        
-        print("<li><a class='fly' href='#'>".$language['ACP_HACKS']."</a>\n");
-				print("<!--[if gte IE 7]><!--></a><!--<![endif]--><!--[if lte IE 6]><table><tr><td><![endif]-->\n");
-        print("<ul class='level4'>\n");
-        print("<li><a href='index.php?page=admin&amp;user=".$CURUSER["uid"]."&amp;code=".$CURUSER["random"]."&do=hacks&action=read'>".$language['ACP_HACKS_CONFIG']."</a></li></ul><!--[if lte IE 6]></td></tr></table></a><![endif]--></li>\n");
-        print("</ul><!--[if lte IE 6]></td></tr></table></a><![endif]--></li>\n");
-    }		
-		
-		if ($CURUSER["view_users"]=="yes")
-		{
-    print("<li><a class='fly' href='#'>".$language["USER_CP"]."</a>\n");
-    print("<!--[if gte IE 7]><!--></a><!--<![endif]--><!--[if lte IE 6]><table><tr><td><![endif]-->\n");
-    print("<ul class='level3'>\n");
-    print("<li><a href='index.php?page=usercp&amp;uid=".$CURUSER["uid"]."'>".$language['MNU_UCP_HOME']."</a></li>\n");
-		
-    print("<li><a class='fly' href='#'>".$language["MNU_UCP_PM"]."</a>\n");
-		print("<!--[if gte IE 7]><!--></a><!--<![endif]--><!--[if lte IE 6]><table><tr><td><![endif]-->\n");
-    print("<ul class='level4'>");
-    print("<li><a href='index.php?page=usercp&uid=".$CURUSER["uid"]."&do=pm&action=list&what=inbox'>".$language['MNU_UCP_IN']."</a></li>\n");
-    print("<li><a href='index.php?page=usercp&uid=".$CURUSER["uid"]."&do=pm&action=list&what=outbox'>".$language['MNU_UCP_OUT']."</a></li>\n");
-    print("<li><a href='index.php?page=usercp&uid=".$CURUSER["uid"]."&do=pm&action=edit&uid=".$CURUSER["uid"]."&what=new'>".$language['MNU_UCP_NEWPM']."</a></li></ul><!--[if lte IE 6]></td></tr></table></a><![endif]--></li>\n");
 
-    print("<li><a class='fly' href='#'>".$language["MNU_UCP_INFO"]."</a>\n");
-		print("<!--[if gte IE 7]><!--></a><!--<![endif]--><!--[if lte IE 6]><table><tr><td><![endif]-->\n");
-    print("<ul class='level4'>\n");
-    print("<li><a href='index.php?page=usercp&do=user&action=change&uid=".$CURUSER["uid"]."'>".$language['MNU_UCP_INFO']."</a></li>\n");
-    print("<li><a href='index.php?page=usercp&do=pwd&action=change&uid=".$CURUSER["uid"]."'>".$language['MNU_UCP_CHANGEPWD']."</a></li>\n");
-    print("<li><a href='index.php?page=usercp&do=pid_c&action=change&uid=".$CURUSER["uid"]."'>".$language['CHANGE_PID']."</a></li></ul><!--[if lte IE 6]></td></tr></table></a><![endif]--></li>\n");
-    print("</ul><!--[if lte IE 6]></td></tr></table></a><![endif]--></li>\n");
-}
-
-		if ($CURUSER["view_forum"]=="yes")
+if ($CURUSER["view_forum"]=="yes")
     {
         if ($GLOBALS["FORUMLINK"]=="" || $GLOBALS["FORUMLINK"]=="internal" || substr($GLOBALS["FORUMLINK"],0,3)=="smf" || $GLOBALS["FORUMLINK"]=="ipb")
             print("<li><a href='index.php?page=forum'>".$language["MNU_FORUM"]."</a></li>\n");
@@ -178,15 +68,25 @@ if ($CURUSER["view_news"]=="yes")
             print("<li><a href='".$GLOBALS["FORUMLINK"]."'>".$language["MNU_FORUM"]."</a></li>\n");
     }
 
-    if ($CURUSER["view_news"]=="yes")
+if ($CURUSER["view_news"]=="yes")
     {
         print("<li><a href='index.php?page=viewnews'>".$language['MNU_NEWS']."</a></li>\n");
     }
-		if ($CURUSER["view_users"]=="yes")
+
+if ($CURUSER["view_users"]=="yes")
 		{    
     print("<li><a href='index.php?page=users'>".$language["MNU_MEMBERS"]."</a><b></b></li>\n");
     }
-/*		if ($CURUSER["view_users"]=="yes")
+
+/* there is already a my panel to edit profiles no need to waste space unless really needed in top menu*/
+
+/*  just tidying up a little here */ 
+
+/*print("<li><a href='#'>".$language['USER_MENU']."</a>\n");
+    print("<ul>\n");
+*/
+	
+/*if ($CURUSER["view_users"]=="yes")
 		{
     print("<li><a class='fly' href='#'>".$language["USER_CP"]."</a>\n");
     print("<!--[if gte IE 7]><!--></a><!--<![endif]--><!--[if lte IE 6]><table><tr><td><![endif]-->\n");
@@ -207,8 +107,10 @@ if ($CURUSER["view_news"]=="yes")
     print("<li><a href='index.php?page=usercp&do=pwd&action=change&uid=".$CURUSER["uid"]."'>".$language['MNU_UCP_CHANGEPWD']."</a></li>\n");
     print("<li><a href='index.php?page=usercp&do=pid_c&action=change&uid=".$CURUSER["uid"]."'>".$language['CHANGE_PID']."</a></li></ul><!--[if lte IE 6]></td></tr></table></a><![endif]--></li>\n");
     print("</ul><!--[if lte IE 6]></td></tr></table></a><![endif]--></li>\n");
-}
-    if ($CURUSER["admin_access"]=="yes")
+} */
+	
+/* same as above there is an admin link so there is not really much need as its accessable via admin link unless its really needed in menu. */	
+/*if ($CURUSER["admin_access"]=="yes")
     {
         require_once(load_language("lang_admin.php"));
         print("<li><a class='fly' href='#'>".$language["ADMIN_ACCESS"]."</a>\n");
@@ -267,12 +169,22 @@ if ($CURUSER["view_news"]=="yes")
         print("<ul class='level4'>\n");
         print("<li><a href='index.php?page=admin&amp;user=".$CURUSER["uid"]."&amp;code=".$CURUSER["random"]."&do=hacks&action=read'>".$language['ACP_HACKS_CONFIG']."</a></li></ul><!--[if lte IE 6]></td></tr></table></a><![endif]--></li>\n");
         print("</ul><!--[if lte IE 6]></td></tr></table></a><![endif]--></li>\n");
-    }*/
-
-    print("</ul><!--[if lte IE 6]></td></tr></table></a><![endif]--></li>\n");
-		print("</ul><!--[if lte IE 6]></td></tr></table></a><![endif]--></li>\n");
+    }*/	
+	
+	if ($CURUSER["uid"]==1 || !$CURUSER)
+{
+    print("<li><a href='index.php?page=login'>".$language["LOGIN"]."</a></li>\n");
+}
+else
+{
+    print("<li><a href='logout.php'>".$language["LOGOUT"]."</a></li>\n");
 }
 
-print("</div>");
+    /*print("</ul></li>\n");
+		print("</ul></li>\n");*/
+}
+
+print("</ul>");
+print("</div>")
 
 ?>
--- style/xbtit_default/main.tpl
+++ style/xbtit_default/main.tpl
@@ -1,110 +1,112 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html<tag:main_rtl /> xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-  <title><tag:main_title /></title>
-  <meta http-equiv="content-type" content="text/html; charset=<tag:main_charset />" />
-  <link rel="stylesheet" href="<tag:main_css />" type="text/css" />
-  <tag:more_css />
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title><tag:main_title /></title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="style/xbtit_default/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="style/xbtit_default/css/modern.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="style/xbtit_default/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+	<tag:more_css />
     <tag:main_jscript />
-
-<if:IS_DISPLAYED_1>
-<!--[if lte IE 7]>
-<style type="text/css">
-#menu ul {display:inline;}
-</style>
-<![endif]-->
-</if:IS_DISPLAYED_1>
+	
 </head>
-<body>
-<div id="main">
-<if:IS_DISPLAYED_2>
-  <div id="logo">
-    <table width="792" align="center" cellpadding="0" cellspacing="0" border="0">
-      <tr>
-        <td class="tracker_logo" align="center" valign="top"></td>
-      </tr>
-    </table></div>
 
-    <TABLE align="center" width="700" cellpadding="0" cellspacing="0" border="0">
-      <TR>
-        <TD valign="top">  
-    <div id="dropdown">
-      <tag:main_dropdown />
-   </div></TD>
-       </TR>
-    </TABLE>
-    <TABLE align="center" width="100%" height="75" cellpadding="0" cellspacing="0" border="0">
-      <TR>
-        <TD valign="top" background="style/xbtit_default/images/spacer.gif"></TD>
-       </TR>
-    </TABLE>
-    <TABLE align="center" width="982" cellpadding="0" cellspacing="0" border="0">
-      <TR>
-        <TD valign="top">
-  <div id="slideIt">
-    <tag:main_slideIt />
-    <div id="header">
-      <table width="100%" align="center" cellpadding="0" cellspacing="0" border="0">
-        <tr>
-          <td valign="top" width="5" rowspan="2"></td>
-          <td valign="top"><tag:main_header /></td>
-          <td valign="top" width="5" rowspan="2"></td>
-        </tr>
-      </table>
-    </div>
-  </div>
-  <script type="text/javascript">
-    var collapse2=new animatedcollapse("header", 800, false, "block")
-  </script>
-  <div id="bodyarea" style="padding:4ex 0 0 0;">  
-    <table border="0" align="center" cellpadding="0" cellspacing="0" width="100%">
-      <tr>
-        <td valign="top" width="5" rowspan="2"></td>
-        <if:HAS_LEFT_COL>
-        <td valign="top" id="col" width="150"><tag:main_left /></td>
-        <td valign="top" width="5" rowspan="2"></td>
-        </if:HAS_LEFT_COL>
-        <td id="mcol" valign="top"><tag:main_content /></td>
-        <if:HAS_RIGHT_COL>
-        <td valign="top" width="5" rowspan="2"></td>
-        <td valign="top" id="col" width="150"><tag:main_right /></td>
-        </if:HAS_RIGHT_COL>
-        <td valign="top" width="5" rowspan="2"></td>
-      </tr>
-    </table>
-    <br />      
-    <table align="center" width="100%" cellpadding="0" cellspacing="0" border="0">
-      <tr>
-        <td valign="top" width="5" rowspan="2"></td>
-        <td id="mcol" valign="top"><tag:main_footer /></td>
-        <td valign="top" width="5" rowspan="2"></td>
-      </tr>
-    </table>
-        <br />
-  </div>
-    </TD>
-      </TR>
-    </TABLE>
-  <div id="footer">
-       <table width="100%" align="center" cellpadding="0" cellspacing="0" border="0">
-         <tr>
-  <td align="center" valign="bottom"><br /><br /><br /><br /><br /><tag:style_copyright />&nbsp;<tag:xbtit_version /><br />
-         <tag:xbtit_debug /></td>
-        </tr><tr>
-                <td class="footer" align="center" valign="bottom"><br /><br /><br /><tag:to_top /></td>
-         </table>
-      </div>
-<else:IS_DISPLAYED_2>
-    <div id="bodyarea" style="padding: 1ex 0ex 0ex 0ex;">  
-<table border="0" align="center" cellpadding="0" cellspacing="0" width="100%">
-    <tr>
-<td valign="top" width="5" style="background: url(images/spacer.gif);" rowspan="2"></td>
-    <td valign="top"><tag:main_content /></td>
-<td valign="top" width="5" style="background: url(images/spacer.gif);" rowspan="2"></td>
-      </tr>
-    </table>
-      </div>
-</if:IS_DISPLAYED_2>
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">XBTIT</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <tag:main_dropdown />
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+<if:IS_DISPLAYED_2>
+<tag:main_header />
+<!-- Content Row -->
+        <div class="row">
+<if:HAS_LEFT_COL>		
+            <!-- Sidebar Column -->
+            <div class="col-md-2">
+<tag:main_left />
 </div>
+</if:HAS_LEFT_COL>
+            <!-- Content Column -->
+			<!-- if right column is live again make the below col-md8 -->
+            <div class="col-md-10">
+<tag:main_content />
+<tag:main_footer /> 
+ </div>
+ <!-- Right side is not needed for now as it repeats some of the top and left -->
+ <noscript>
+ <if:HAS_RIGHT_COL>
+  <!-- Content Column -->
+            <div class="col-md-2">
+<tag:main_right /> 
+</div>
+</if:HAS_RIGHT_COL>
+</noscript>
+</div>
+<else:IS_DISPLAYED_2>
+<tag:main_content />
+</if:IS_DISPLAYED_2>
+
+
+        <!-- /.row -->
+    <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+				    
+                    <p>Copyright &copy; 2016 BTITeam / DPWS Media LTD <noscript><tag:style_copyright />&nbsp;<tag:xbtit_version /></noscript></p>
+					<p><tag:xbtit_debug /></p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="style/xbtit_default/js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="style/xbtit_default/js/bootstrap.min.js"></script>
+
 </body>
 </html>
