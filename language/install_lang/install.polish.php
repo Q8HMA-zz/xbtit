28307f5f9bb5eeb457140c9eb7211a5c452eca6b
KingCobra58 <kingcobra@xbtitfm.com>
2016-04-14 10:49:15 -0400

    updated copyrights in the install language files

--- language/install_lang/install.polish.php
+++ language/install_lang/install.polish.php
@@ -2,7 +2,7 @@
 /////////////////////////////////////////////////////////////////////////////////////
 // xbtit - Bittorrent tracker/frontend
 //
-// Copyright (C) 2004 - 2007  Btiteam
+// Copyright (C) 2004 - 2015  Btiteam
 //
 //    This file is part of xbtit.
 //
